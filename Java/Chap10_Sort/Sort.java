0f5f0376e0ccdb999ad753146e8e30e884e3e758
YoungKoungKim <ykk0413@gmail.com>
2017-07-26 20:01:04 +0900

    Update Sort.java
    update merge, counting, quick, bucket sort

--- Java/Chap10_Sort/Sort.java
+++ Java/Chap10_Sort/Sort.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Sort {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.printf("정렬 할 행렬 입력 >> ");
 		String arr = scan.nextLine();
 		String strArr[] = arr.split(" ");
@@ -29,15 +29,31 @@ public class Sort {
 		} else if (select == 4) {
 			ShellSort(A);
 		} else if (select == 5) {
-			MergeSort(A, 0, A.length);
+			int size = A.length;
+			Merge m = new Merge();
+			m.mergeSort(A, 0, size-1);
 		} else if (select == 6) {
 
 		} else if (select == 7) {
-
+			Quick q = new Quick();
+			q.quickSort(A, 0, 7);
 		} else if (select == 8) {
-
+			int[] B = new int[A.length];
+			
+			int max = A[0];
+			for (int num : A) {
+				if (max < num)
+					max = num;
+			}
+			System.out.println("max 값 : " + max);
+			
+			CountingSort(A, B, max);
+			
+			for (int i = 0; i < A.length; i++) {
+				A[i] = B[i];
+			}
 		} else if (select == 9) {
-
+			BucketSort(A);
 		} else if (select == 0) {
 
 		}
@@ -50,6 +66,7 @@ public class Sort {
 
 	}
 
+//	버블정렬
 	static void BubbleSort(int[] A) {
 		int cnt = 0;
 		boolean swapped = true;
@@ -72,6 +89,7 @@ public class Sort {
 		}
 	}
 
+//	선택정렬
 	static void SelectionSort(int[] A) {
 		int cnt = 0;
 		int min;
@@ -143,6 +161,7 @@ public class Sort {
 		}
 	}
 
+//	셸정렬
 	static void ShellSort(int[] A) {
 		int h = A.length / 2;
 		int temp;
@@ -164,49 +183,148 @@ public class Sort {
 		}
 	}
 	
-	static void MergeSort(int[] A, int left, int right) {
-		int mid;
-		if (left < right) {
-			mid = (right + left) / 2;
-			MergeSort(A, left, mid);
-			MergeSort(A, mid+1, right);
-			merge(A, left, mid, right);
+//	계수정렬
+	static void CountingSort(int A[], int B[], int K) {
+		int[] C = new int[K + 1];
+
+		for (int j = 0; j < A.length; j++) {
+			C[A[j]] = C[A[j]] + 1;
 		}
-	}
-	
-	static void merge(int[] A, int left, int mid, int right) {
-		int[] temp = new int[30];
-		int i = left;
-		int j = mid + 1;
-		int k = left;
-		int t;
-		int cnt = 0;
-		
-		while (i <= mid && j <= right) {
-			if (A[i] <= A[j])
-				temp[k] = A[i++];
-			else
-				temp[k] = A[j++];
-			k++;
-		}
-		
-		if (i > mid) {
-			for (t = j; t <= right; t++, k++)
-				temp[k] = A[t];
-		} else {
-			for (t = i; i <= mid; t++, k++) {
-				temp[k] = A[t];
+
+		for (int i = 0; i < K+1; i++) {
+			if (i == 0) {
+				C[i]--;
+			} else {
+				C[i] = C[i] + C[i-1];
 			}
 		}
 		
-		for (t = left; t <= right; t++) {
-			A[t] = temp[t];
+		for (int k = A.length-1; k >= 0; k--) {
+			B[C[A[k]]] = A[k];
+			C[A[k]] = C[A[k]] - 1;
 		}
 		
-		System.out.printf(++cnt + "회차 : ");
-		for (int a : A) {
-			System.out.printf(a + " ");
+	}
+	
+//	버킷정렬
+	static void BucketSort(int A[]) {
+		int max = A[0];
+		for (int num : A) {
+			if (max < num)
+				max = num;
+		}
+		int BUCKETS = max+1;
+		
+		int i, j, k;
+		int[] buckets = new int[BUCKETS];
+		
+		for (i = 0; i < A.length; i++)
+			++buckets[A[i]];
+		for (i = 0, j = 0; j < BUCKETS; j++)
+			for (k = buckets[j]; k > 0; --k)
+				A[i++] = j;
+	}
+
+}
+
+// 병합정렬 클래스
+class Merge {
+	private int sorted[] = new int[30];
+	private int cnt = 0;
+	
+	public void merge(int a[], int m, int middle, int n) {
+		int size = a.length;
+		int i, j, k, t;
+		i = m;
+		j = middle + 1;
+		k = m;
+		
+		while (i <= middle && j <= n) {
+			if (a[i] <= a[j])
+				sorted[k] = a[i++];
+			else
+				sorted[k] = a[j++];
+			k++;
+		}
+		
+		if (i > middle) {
+			for (t = j; t <= n; t++, k++)
+				sorted[k] = a[t];
+		} else {
+			for (t = i; t <= middle; t++, k++)
+				sorted[k] = a[t];
+		}
+		
+		for (t = m; t <= n; t++)
+			a[t] = sorted[t];
+		
+		System.out.printf(++cnt + "회차 >> ");
+		for(t = 0; t < size; t++)
+			System.out.printf("%3d", a[t]);
+		System.out.println();
+	}
+	
+	public void mergeSort(int a[], int m, int n) {
+		int middle;
+		if (m < n) {
+			middle = (m+n) / 2;
+			mergeSort(a, m, middle);
+			mergeSort(a, middle+1, n);
+			merge(a, m, middle, n);
+		}
+	}
+
+}
+
+// 퀵정렬 클래스
+class Quick {
+	int i = 0;
+
+	public int partition(int a[], int begin, int end) {
+		int pivot, temp, L, R, t;
+
+		L = begin;
+		R = end;
+		pivot = (begin + end) / 2;
+		System.out.printf("%d 단계 : pivot = %d\n", ++i, a[pivot]);
+
+		while (L < R) {
+			while ((a[L] < a[pivot]) && (L < R))
+				L++;
+			while ((a[R] >= a[pivot]) && (L < R))
+				R--;
+			if (L < R) {
+				temp = a[L];
+				a[L] = a[R];
+				a[R] = temp;
+
+				if (L == pivot) {
+					for (t = 0; t < a.length; t++) {
+						System.out.printf("%3d", a[t]);
+					}
+					System.out.println();
+
+					return R;
+				}
+			}
+		}
+
+		temp = a[pivot];
+		a[pivot] = a[R];
+		a[R] = temp;
+		for (t = 0; t < a.length; t++) {
+			System.out.printf("%3d", a[t]);
 		}
 		System.out.println();
- 	}
+		return L;
+	}
+
+	public void quickSort(int a[], int begin, int end) {
+		if (begin < end) {
+			int p;
+			p = partition(a, begin, end);
+			quickSort(a, begin, p - 1);
+			quickSort(a, p + 1, end);
+		}
+	}
 }
